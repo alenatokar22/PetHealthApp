3a96a33fd2c8198ee1438837f12833ac8fc959bf
Alyona <alenatokar22@gmail.com>
2025-11-20 11:24:45 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/PetHealthApp/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
   plugins: [react()],
 });
-
